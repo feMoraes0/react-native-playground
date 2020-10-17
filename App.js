d0c891caae9adae811c2732753c6e5b8f705608d
Fernando de Moraes Lopes <fernandomoraes.lopes@gmail.com>
2020-10-17 18:32:27 -0300

    Cleaning texts

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { StyleSheet, View, Text, StatusBar } from 'react-native';
 
 const App = () => {
   return (
