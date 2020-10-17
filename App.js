914260eaefe046f935e8ab05b8fc8e05529ed87a
Fernando de Moraes Lopes <fernandomoraes.lopes@gmail.com>
2020-10-17 20:11:45 -0300

    Modifications made following documentation, but failed

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { StyleSheet, View, Text, StatusBar } from 'react-native';
+import React, {useEffect} from 'react';
+import {StyleSheet, View, Text, StatusBar, Button} from 'react-native';
+import PushNotification from 'react-native-push-notification';
+
+PushNotification.configure({
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  popInitialNotification: true,
+  requestPermissions: true,
+});
 
 const App = () => {
+  useEffect(() => {
+    PushNotification.localNotification({
+      title: 'Hello',
+      message: 'World',
+    });
+  });
+
+  testPushNotification = () => {
+    console.log('Hello world 2');
+  };
+
   return (
     <>
       <StatusBar barStyle="light-content" />
       <View style={styles.body}>
-        <Text style={styles.text}>React Native project to practise push notification and background execution</Text>
+        <Text style={styles.text}>
+          React Native project to practise push notification and background
+          execution
+        </Text>
+        <Button
+          onPress={() => {
+            testPushNotification();
+          }}
+          title={'hello world'}
+        />
       </View>
     </>
   );
@@ -15,16 +46,16 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    backgroundColor: "#FD4949",
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#FD4949',
     padding: 60,
   },
   text: {
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 25,
-    color: "#FFFFFF"
-  }
+    color: '#FFFFFF',
+  },
 });
 
 export default App;
