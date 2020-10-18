91e52d70c18bc6672d6d77eb586780b7e94d3487
Fernando de Moraes Lopes <fernandomoraes.lopes@gmail.com>
2020-10-18 16:58:59 -0300

    Added react-native-push-notification changes

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { StyleSheet, View, Text, StatusBar } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+import {show} from './src/helpers/notify';
 
 const App = () => {
+  const notify = () => {
+    console.log('useEffect started');
+    show('hello', 'message');
+    console.log('useEffect finished');
+  };
+
   return (
     <>
-      <StatusBar barStyle="light-content" />
+      <StatusBar barStyle="dark-content" />
       <View style={styles.body}>
-        <Text style={styles.text}>React Native project to practise push notification and background execution</Text>
+        <Text style={styles.text}>
+          React Native project to practise local notification
+        </Text>
+        <TouchableOpacity onPress={() => notify()}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Notify!</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </>
   );
@@ -15,16 +35,27 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    backgroundColor: "#FD4949",
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#292830',
     padding: 60,
   },
   text: {
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 25,
-    color: "#FFFFFF"
-  }
+    color: '#18B6EC',
+  },
+  button: {
+    backgroundColor: '#18B6EC',
+    padding: 15,
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  buttonText: {
+    fontSize: 17,
+  },
 });
 
 export default App;
