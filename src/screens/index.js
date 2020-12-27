af15252dfb1f64feea90fe002a0d34c6392b1c23
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-12-27 20:58:01 -0300

    Adds svg icons and clean project structure

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {SafeAreaView, View, TouchableOpacity, StyleSheet} from 'react-native';
+import Menu from '../../assets/icons/menu.svg';
 
 const MenuAnimation = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.header} />
-      <Text style={styles.text}> Scaffold Screen </Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Menu color="#000" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  header: {
+    paddingHorizontal: 15.0,
   },
   text: {
     fontSize: 18.0,
